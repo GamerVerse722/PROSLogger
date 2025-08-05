648bc735a8fdd01a4ffde2770ef1f0952a0bb7a7
GamerVerse <cristian987654321rios@gmail.com>
2025-08-05 10:16:27 -0500

    removed color

--- include/PROSLogger/PROSLogger.hpp
+++ include/PROSLogger/PROSLogger.hpp
@@ -21,16 +21,6 @@ namespace PROSLogger {
         }
     }
 
-    inline std::string levelToColor(const LogLevel level) {
-        switch (level) {
-            case DEBUG: return "\033[0;34m";
-            case INFO: return "\033[0;32m";
-            case WARN: return "\033[0;33m";
-            case ERROR: return "\033[0;31m";
-            default: return "";
-        }
-    }
-
     struct LoggerEvent {
         LogLevel level;
         std::string groupId;
